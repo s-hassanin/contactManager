3636fdd5cb84b3823f2b3d8b241437c20087a398
Mike Burke <michaelburke.eh@gmail.com>
2023-05-23 20:37:16 -0400

    initial code.js commit for branching

---
+++ api/code.js
@@ -0,0 +1,39 @@
+// TODO
+//
+// All!
+
+// Login
+function loginUser()
+{
+
+}
+
+// Register
+function registerUser()
+{
+
+}
+
+// Add
+function addContact()
+{
+
+}
+
+// Search
+function searchContact()
+{
+
+}
+
+// Edit
+function editContact()
+{
+
+}
+
+// Delete
+function deleteContact()
+{
+
+}
