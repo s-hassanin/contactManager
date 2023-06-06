1850a62da54ac0f20a5e789f8d7a9ce5826720c0
s-hassanin <82418172+s-hassanin@users.noreply.github.com>
2023-06-06 13:24:35 -0400

    Update COP4331.sql

--- MySQL/COP4331.sql
+++ MySQL/COP4331.sql
@@ -18,12 +18,12 @@ CREATE TABLE `COP4331`.`Users`
 CREATE TABLE `COP4331`.`Contacts`
 (
     `ContactID` INT NOT NULL AUTO_INCREMENT ,
-    `User_ID` INT NOT NULL,
+    `UserID` INT NOT NULL,
     `FirstName` VARCHAR(50) NOT NULL DEFAULT '' ,
     `LastName` VARCHAR(50) NOT NULL DEFAULT '' ,
     `Phone` VARCHAR(50) NOT NULL DEFAULT '' ,
     `Email` VARCHAR(50) NOT NULL DEFAULT '' ,
-    CONSTRAINT FK_ID FOREIGN KEY (User_ID) REFERENCES Users(ID),
+    CONSTRAINT FK_ID FOREIGN KEY (UserID) REFERENCES Users(ID),
     PRIMARY KEY (`ContactID`)
 ) ENGINE = InnoDB;
 
@@ -36,18 +36,20 @@ insert into Users (FirstName,LastName,Login,Password) VALUES ('Korinne','Ramchar
 insert into Users (FirstName,LastName,Login,Password) VALUES ('Schander','Joseph','SchanderJ','baseBall');
 
 
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Rick', 'Leinecker','4078230169','Richard.Leinecker@ucf.edu',1);
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Sam', 'Smith','4078237799','Sam.smith@gmail.com',1);
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('William', 'Jiovani','4079907799','w.jio@gmail.com',1);
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Kylie', 'Esteleniski','4076007129','kylieE@gmail.com',1);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Rick', 'Leinecker','4078230169','Richard.Leinecker@ucf.edu',1);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Sam', 'Smith','4078237799','Sam.smith@gmail.com',1);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('William', 'Jiovani','4079907799','w.jio@gmail.com',1);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Kylie', 'Esteleniski','4076007129','kylieE@gmail.com',1);
 
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Luis', 'Gonzalez','4077651234','lgon7334@gmail.com',2);
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Nathan', 'Carter','4074653578','nathan342@gmail.com',2);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Luis', 'Gonzalez','4077651234','lgon7334@gmail.com',2);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Nathan', 'Carter','4074653578','nathan342@gmail.com',2);
 
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Adam', 'Wilson','9973451243','Adamw490@outlook.com',3);
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Juan', 'Martinez','9978904357','jmartinez77@outlook.com',3);
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Jessica', 'Brown','8892134454','jbrown890@hotmail.com',3);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Adam', 'Wilson','9973451243','Adamw490@outlook.com',3);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Juan', 'Martinez','9978904357','jmartinez77@outlook.com',3);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Jessica', 'Brown','8892134454','jbrown890@hotmail.com',3);
 
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Aly', 'buradi','7198806655','alya22@gmail.com',4);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Aly', 'buradi','7198806655','alya22@gmail.com',4);
 
-insert into Contacts(FirstName,LastName,Phone,Email,User_ID) VALUES ('Jasmine', 'Cooper','6658902134','jcooper11@gmail.com',5);
+insert into Contacts(FirstName,LastName,Phone,Email,UserID) VALUES ('Jasmine', 'Cooper','6658902134','jcooper11@gmail.com',5);
+
+
